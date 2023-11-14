d794dbcd60a4bb11110983a1478ddb9f8ab0f17c
GoodChoisWork <goodchois.work@gmail.com>
2023-11-14 23:33:06 +0800

    added tags and classes

--- screens/HowToPlay.js
+++ screens/HowToPlay.js
@@ -1,13 +1,18 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from "react-native-safe-area-context";
 const HowToPlay = () => {
   return (
     <SafeAreaView>
-      <Text>How To Play</Text>
-        <view>
-          <text>If you're playing roulette strictly for entertainment purposes and not for real money, you can organize a friendly game using simulated or play-money chips. Here's a guide on how to set up and play a roulette game for entertainment:</text>
-        </view> 
+      <View>
+        <Text style={styles.title}>How To Play</Text>
+      </View>
+
+        <View>
+          <Text style={styles.textIntro}>If you're playing roulette strictly for entertainment purposes and not for real money,
+             you can organize a friendly game using simulated or play-money chips. Here's a guide on how
+              to set up and play a roulette game for entertainment:</Text>
+        </View> 
 
     </SafeAreaView>
   )
@@ -17,6 +22,20 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#45271e",
+  },
+
+  title: {
+
+    display: flex,
+    justifyContent: center,
+    alignItems: center,
+    
+  },
+
+  textIntro: {
+
+    fontSize: 50,
+
   },
 })
 export default HowToPlay
