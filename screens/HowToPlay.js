142b556ad14d47edda66bef8a72eb133c2004945
GoodChoisWork <goodchois.work@gmail.com>
2023-11-14 23:19:19 +0800

    Added introduction info text

--- screens/HowToPlay.js
+++ screens/HowToPlay.js
@@ -4,9 +4,19 @@ import { SafeAreaView } from "react-native-safe-area-context";
 const HowToPlay = () => {
   return (
     <SafeAreaView>
-      <Text>HowToPlay</Text>
+      <Text>How To Play</Text>
+        <view>
+          <text>If you're playing roulette strictly for entertainment purposes and not for real money, you can organize a friendly game using simulated or play-money chips. Here's a guide on how to set up and play a roulette game for entertainment:</text>
+        </view> 
+
     </SafeAreaView>
   )
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#45271e",
+  },
+})
 export default HowToPlay
