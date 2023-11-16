65373c2b8a95ee4333a29125e707dccc9c53643c
GoodChoisWork <goodchois.work@gmail.com>
2023-11-17 06:49:37 +0800

    Add text in about

--- screens/About.js
+++ screens/About.js
@@ -4,9 +4,28 @@ import { SafeAreaView } from "react-native-safe-area-context";
 const About = () => {
   return (
     <SafeAreaView>
-      <Text>About</Text>
+      <View style={styles.flexTitle}>
+          <Text style={styles.title}>About</Text>
+        </View>
     </SafeAreaView>
   )
 }
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#45271e",
+  },
+
+flexTitle: {
+
+  padding: 20,
+  fontSize: 40,
+
+},
+
+})
 
 export default About
